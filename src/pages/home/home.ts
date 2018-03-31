1b31f4af58dda036b761294450db5e7a42059f15
Fulcrand <guilhem.fulcrand@epitech.eu>
2018-03-31 08:10:31 +0000

    [ADD]Student Homepage

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { NavController, NavParams, ToastController, AlertController, LoadingController, App, ModalController, Events } from 'ionic-angular';
+import { DatePipe } from '@angular/common';
+import { Ng2Cable } from "ng2-cable";
+import { Http } from '@angular/http';
+
+import { AuthService } from '../../services/auth.service';
+import { DataService } from '../../services/data.service';
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+	providers:[DatePipe]
 })
 export class HomePage {
+
+	subSettings: string = 'home';
 
   constructor(public navCtrl: NavController) {
 
